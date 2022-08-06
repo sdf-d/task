035be9b8306556f236891bd574db4f9db9a54be8
Deniz Hacisuleyman <sddfe@fherh>
2022-08-06 19:50:13 +0300

    salesrep endpoint set

--- routes/salesrep.js
+++ routes/salesrep.js
@@ -10,14 +10,16 @@ router.get('/', async (req,res) => {
     console.log("salesrep endpoint called")
     const result = await axios.get('http://localhost:3000/countries')
     var data = result.data
+    // var groupedData = groupBy(data, 'region')
+    // console.log(JSON.stringify(groupedData, null, 2))
+    var newdata = businessLogic.solveProblem1(data)
     // const isRegionSpecified = req.query.region !== undefined
     // const region = req.query.region 
     // const filter = isRegionSpecified ? {region : region} : {}
     // const all = await Country.find(filter, {_id : 0}).lean()
     // resstring = isRegionSpecified ? `<h2>Welcomeeeeeeee ` + req.query.region + `</h2>` : `<h2>Welcomeeee</h2>`
     // res.json(all)
-    res.json(data)
-    // res.send(resstring)
+    res.json(newdata)
 })
 
 
