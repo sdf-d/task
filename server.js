4844c35e5a49f867cbf581c586f5fbb4f14df5b8
Deniz Hacisuleyman <sddfe@fherh>
2022-08-07 02:51:19 +0300

    removed test files

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 require('dotenv').config()
 const express = require('express')
-const path = require('path')
-
-
-
-
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URL)
@@ -15,60 +10,14 @@ console.log("mongo successful")
 var app = express()
 
 
-
-
 const countriesRouter = require('./routes/countries')
 app.use('/countries', countriesRouter)
 const salesrepRouter = require('./routes/salesrep')
 app.use('/salesrep', salesrepRouter)
 const optimalRouter = require('./routes/optimal')
 app.use('/optimal', optimalRouter)
-// const testRouter = require('./routes/test')
-// app.use('/test', testRouter)
 
 
-
-
-
-// app.get('/', (req,res) => {
-//     console.log("hellooo")
-//     resstring = req.query.region  !== undefined ? `<h2>Welcomeeeeeeeee ` + req.query.region + `</h2>` : `<h2>Welcomeeeeeeeee</h2>`
-//     res.send(resstring)
-// })
-
-
-
-
-// task = async () => {
-//     const filter = {};
-//     const all = await Country.find(filter, {_id : 0}).lean()
-//     // .lean().exec(function (err, countries) {
-//         // console.log(countries);
-//     // });
-//     console.log(JSON.stringify(all))
-// }
-
-// task = async () => {
-//     const result = await axios.get('http://localhost:3000/countries')
-//     console.log(JSON.stringify(result.data[0]))
-// }
-
-// task()
-
-
-
-// app.listen(3000, () => {
-//     console.log("Server at 3000!")
-// }).on("error", (err) => {
-//     process.once('SIGUSR2', function () {
-//         process.kill(process.pid, 'SIGUSR2');
-//     });
-
-//     process.on('SIGINT', function () {
-//         // this is only called on ctrl+c, not restart
-//         process.kill(process.pid, 'SIGINT');
-//     });
-// })
 
 app.listen(3000, () => {
     console.log("Server at 3000!")
--- test2.js
+++
@@ -1,7 +0,0 @@
-const utils = require('./utils')
-sdf = {}
-sdf.aaa = 3
-sdf.AAA = 5
-
-utils.prettyPrint(sdf)
-console.log(utils.prettyPrint(sdf))
