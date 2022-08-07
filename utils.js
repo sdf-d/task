fd43de85040ba6e646ae06df378f3b8248a490b3
Deniz Hacisuleyman <sddfe@fherh>
2022-08-07 22:03:17 +0300

    removed comments

--- utils.js
+++ utils.js
@@ -1,25 +1,7 @@
-// function sleep(ms) {
-//     return new Promise(resolve => setTimeout(resolve, ms));
-// }
-
 function prettyPrint(data) {
     return JSON.stringify(data, null, 2)
 }
 
-// const groupBy = (items, key) => items.reduce(
-//     (result, item) => ({
-//       ...result,
-//       [item[key]]: [
-//         ...(result[item[key]] || []),
-//         item,
-//       ],
-//     }), 
-//     {},
-//   );
-
-
 module.exports = {
-    // sleep,
-    // groupBy,
     prettyPrint
 }
