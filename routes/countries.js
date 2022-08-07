7cf6dab483b30612147553182313d6f408028f33
Deniz Hacisuleyman <sddfe@fherh>
2022-08-07 20:19:06 +0300

    error handling added

--- routes/countries.js
+++ routes/countries.js
@@ -4,14 +4,18 @@ const Country = require('../models/country')
 
 
 router.get('/', async (req, res) => {
-    console.log("countries endpoint called")
-    const isRegionSpecified = req.query.region !== undefined
-    const region = req.query.region?.toUpperCase()
-    const filter = isRegionSpecified ? { region: region } : {}
-    const countries = await Country.aggregate([
-        { $project: { region: { $toUpper: '$region' }, name: 1, _id: 0 } },
-        { $match: filter }])
-    res.json(countries)
+    try {
+        console.log("countries endpoint called")
+        const isRegionSpecified = req.query.region !== undefined
+        const region = req.query.region?.toUpperCase()
+        const filter = isRegionSpecified ? { region: region } : {}
+        const countries = await Country.aggregate([
+            { $project: { region: { $toUpper: '$region' }, name: 1, _id: 0 } },
+            { $match: filter }])
+        res.json(countries)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 })
 
 
--- routes/optimal.js
+++ routes/optimal.js
@@ -5,12 +5,16 @@ const businessLogic = require('../businessLogic')
 const utils = require('../utils')
 
 
-router.get('/', async (req,res) => {
-    console.log("salesrep endpoint called")
-    const result = await axios.get('http://localhost:3000/countries')
-    var data = result.data
-    var solvedData = businessLogic.solveProblem2(data)
-    res.json(solvedData)
+router.get('/', async (req, res) => {
+    try {
+        console.log("salesrep endpoint called")
+        const result = await axios.get('http://localhost:3000/countries')
+        var data = result.data
+        var solvedData = businessLogic.solveProblem2(data)
+        res.json(solvedData)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 })
 
 
