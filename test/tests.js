1325037fdfaa4a5386fe7225a258c89443e682cf
Deniz Hacisuleyman <sddfe@fherh>
2022-08-08 10:15:05 +0300

    test added

--- test/tests.js
+++ test/tests.js
@@ -5,15 +5,15 @@ var businessLogic = require('../businessLogic')
 
 testCountries0_0 = [
     {
-        country: "c0",
+        name: "c0",
         region: "r0"
     },
     {
-        country: "c1",
+        name: "c1",
         region: "r0"
     },
     {
-        country: "c2",
+        name: "c2",
         region: "r0"
     }
 ]
@@ -26,10 +26,30 @@ testSolutions0_0 = [
     }
 ]
 
+
+
+testSolutions1_0 = [
+    {
+        region: "r0",
+        countryList: ["c0", "c1", "c2"],
+        countryCount: 3
+    }
+]
+
+
 describe('SolveProblem0', function () {
     describe('#Test0', function () {
         it('should be 1,1', function () {
             expect(businessLogic.solveProblem1(testCountries0_0)).to.deep.equal(testSolutions0_0);
+        });
+
+    });
+});
+
+describe('SolveProblem1', function () {
+    describe('#Test0', function () {
+        it('c0, c1, c2', function () {
+            expect(businessLogic.solveProblem2(testCountries0_0)).to.deep.equal(testSolutions1_0);
         });
 
     });
